dab72f15f0768fc28a4c3027d61e23916df7bde8
Jason Liu <111265664+liug26@users.noreply.github.com>
2024-04-20 21:39:00 -0700

    Switch to React from Angular

--- dist/me/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-B6WKKGSR.mjs";import"./chunk-FC5RJFY7.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ src/components/appbar.js
@@ -0,0 +1,83 @@
+import React, { useEffect } from 'react';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import MuiAppBar from '@mui/material/AppBar';
+import MuiToolbar from '@mui/material/Toolbar';
+
+const leftMargin = '3rem';
+const centerText = 'CZ';
+const fontFamily = 'Arapey';
+
+function AppBar(props) {
+    useEffect(() => {
+        const appBar = document.getElementById('app-bar');
+        if (props.enableTopTransparent === false && appBar != null) {
+            appBar.style.backgroundColor = 'white';
+            appBar.style.color = 'black';
+        }
+        const onScroll = () => {
+            if (appBar != null)
+                if ((props.enableTopTransparent === undefined || props.enableTopTransparent) && window.scrollY === 0) {
+                    appBar.style.backgroundColor = 'transparent';
+                    appBar.style.color = 'white';
+                } else {
+                    appBar.style.backgroundColor = 'white';
+                    appBar.style.color = 'black';
+                }
+        };
+        window.addEventListener("scroll", onScroll);
+        return () => {
+            window.removeEventListener("scroll", onScroll);
+        };
+    }, []);
+
+    const menuLinksHTML = [];
+    for (let menuLink of props.menuLinks) {
+        if (menuLink.link === '') {
+            menuLinksHTML.push(
+                <Link
+                    key={menuLink.label}
+                    variant='h6'
+                    underline='none'
+                    color='inherit'
+                    onClick={menuLink.onClick}
+                    tabIndex={0}
+                    component='button'
+                    sx={{ marginLeft: leftMargin }}
+                    fontFamily={fontFamily}
+                >
+                    {menuLink.label}
+                </Link>
+            );
+        } else {
+            menuLinksHTML.push(
+                <Link
+                    variant='h6'
+                    underline='none'
+                    color='inherit'
+                    href={menuLink.link}
+                    sx={{ marginLeft: leftMargin }}
+                    fontFamily={fontFamily}
+                >
+                    {menuLink.label}
+                </Link>
+            );
+        }
+    }
+
+    return (
+        <MuiAppBar id='app-bar' elevation={0} position='fixed' sx={{ backgroundColor: 'transparent' }}>
+            <MuiToolbar sx={{ justifyContent: 'space-between' }}>
+                <Box sx={{ flex: 1 }} />
+                <Link href='/' underline='none' variant='h4' color='inherit' fontFamily={fontFamily}>
+                    {centerText}
+                </Link>
+                <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
+                    {menuLinksHTML}
+                </Box>
+            </MuiToolbar>
+        </MuiAppBar>
+    );
+}
+
+export default AppBar;
